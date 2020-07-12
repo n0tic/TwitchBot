c9cac9fd30ef8a222b787b7b3b9577ed905187a5
N0tiC <n0tic@live.se>
2020-07-12 13:28:47 +0200

    Add files via upload
    API update, Crash prevention, Config Options.

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php
-if(isset($_GET['ch'], $_GET['u']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch'])) {
+if(isset($_GET['ch'], $_GET['bot'], $_GET['u']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch']) && $_GET['bot'] != "" && ctype_alnum($_GET['bot'])) {
 	header('Content-Type: application/json;charset=utf-8');
 	$data = json_decode(file_get_contents('php://input'), true);
         
-	$fp = fopen($_GET['ch'].'.json', 'w') or die("NoAccess");
+	$fp = fopen($_GET['bot'].'_'.$_GET['ch'].'.json', 'w') or die("NoAccess");
 	if(fwrite($fp, json_encode($data))) {
-		echo "OK";
+		die("OK");
 	} else {
-		echo "NoWrite";
+		die("NoWrite");
 	}
 	fclose($fp);
 }
-else if(isset($_GET['ch'], $_GET['d']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch']))
+else if(isset($_GET['ch'], $_GET['bot'], $_GET['d']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch']) && $_GET['bot'] != "" && ctype_alnum($_GET['bot']))
 {
     header('Content-Type: application/json;charset=utf-8');
     $data = NULL;
 	
-	if(file_exists($_GET['ch'].'.json'))
+	if(file_exists($_GET['bot'].'_'.$_GET['ch'].'.json'))
 	{
-		if(file_get_contents($_GET['ch'].'.json'))
+		if(file_get_contents($_GET['bot'].'_'.$_GET['ch'].'.json'))
 		{
-			$data = file_get_contents($_GET['ch'].'.json');
+			$data = file_get_contents($_GET['bot'].'_'.$_GET['ch'].'.json');
 		}
 		else
 		{
 			$data = "NoContent";
 		}
-		echo $data;
+		die($data);
 	}
     else
 	{
-		echo "NoFile";
+		die("NoFile");
 	}
 }
+die("This should not be visible.");
 ?>
