3ea71b2c849edcc3872bcd9422a077ebb966bd9f
N0tiC <n0tic@live.se>
2020-07-16 03:47:29 +0200

    Security addition
    Added data checks before saving data. Simple size checks.

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 if(isset($_GET['ch'], $_GET['bot'], $_GET['u']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch']) && $_GET['bot'] != "" && ctype_alnum($_GET['bot'])) {
 	header('Content-Type: application/json;charset=utf-8');
 	$data = json_decode(file_get_contents('php://input'), true);
-        
-	$fp = fopen($_GET['bot'].'_'.$_GET['ch'].'.json', 'w') or die("NoAccess");
-	if(fwrite($fp, json_encode($data))) {
-		die("OK");
-	} else {
-		die("NoWrite");
+	
+	if(Count($data) == 2 && Count($data['hungry']) == 5 && Count($data['deaths']))
+	{
+		$fp = fopen($_GET['bot'].'_'.$_GET['ch'].'.json', 'w') or die("NoAccess");
+		if(fwrite($fp, json_encode($data))) {
+			die("OK");
+		} else {
+			die("NoWrite");
+		}
+		fclose($fp);
 	}
-	fclose($fp);
+	else die("The data is invalid.");
 }
 else if(isset($_GET['ch'], $_GET['bot'], $_GET['d']) && $_GET['ch'] != "" && ctype_alnum($_GET['ch']) && $_GET['bot'] != "" && ctype_alnum($_GET['bot']))
 {
